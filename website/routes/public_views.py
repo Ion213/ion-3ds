89aefd406c098dd43603da7eb470bab6dab2111f
ION Panio <panioaldrich@gmail.com>
2024-04-28 14:14:03 +0800

    database
    added database

--- website/routes/public_views.py
+++ website/routes/public_views.py
@@ -22,6 +22,7 @@ public_views = Blueprint('public_views', __name__)
 @public_views.route('/', methods=['GET', 'POST'])
 def home():
     games = Games.query.all()
+    games = sorted(games, key=lambda x: x.game_name.lower())
     return render_template('home.html', games=games)
 
 @public_views.route('/about', methods=['GET', 'POST'])
